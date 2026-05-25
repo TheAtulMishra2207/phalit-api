510c9073ef8dd5478b156b3e34dbb90c1670130b
TheAtulMishra2207 <136453245+TheAtulMishra2207@users.noreply.github.com>
2026-05-25 20:01:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from routes_kundalis import router as kundalis_router
 from routes_auth import router as auth_router
 from routes_me import router as me_router
 from fastapi import FastAPI, HTTPException
@@ -17,6 +18,7 @@ from typing import Any, Dict
 app = FastAPI(title="Phalit.ai Chart Engine", version="1.0.0")
 app.include_router(me_router)
 app.include_router(auth_router)
+app.include_router(kundalis_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
